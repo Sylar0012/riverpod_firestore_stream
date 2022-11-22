07784e129cdb2027f62e1001630a0f90b8e06692
syalr0012 <kmc42692@naver.com>
2022-11-22 15:26:01 +0900

    firestore insert 연습

--- lib/domain/chat/chat_firestore_repository.dart
+++ lib/domain/chat/chat_firestore_repository.dart
@@ -1,3 +1,16 @@
-//Provider ( 메모리에 띄우기 위한 Provider )
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_firestore_stream/dto/chat/chat_req_dto.dart';
 
-class ChatFireStoreRepository {}
+//Provider ( 메모리에 띄우기 위한 Provider )
+final chatFireStoreRespositoryProvider = Provider((ref) {
+  return ChatFireStoreRepository();
+});
+
+class ChatFireStoreRepository {
+  final db = FirebaseFirestore.instance;
+
+  Future<DocumentReference> insert(ChatInsertReqDto dto) {
+    return db.collection("chat").add(dto.toJson());
+  }
+}
--- lib/view/pages/chat_room/chat_room_page.dart
+++ lib/view/pages/chat_room/chat_room_page.dart
@@ -1,14 +1,28 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_firestore_stream/domain/chat/chat_controller.dart';
+import 'package:riverpod_firestore_stream/dto/chat/chat_req_dto.dart';
 
-class ChatRoomPage extends StatelessWidget {
+class ChatRoomPage extends ConsumerWidget {
   const ChatRoomPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final chatController = ref.read(chatControllerProvider);
     return Scaffold(
       appBar: _bulidAppBar(),
       body: _bulidListView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Random random = Random();
+          ChatInsertReqDto dto = ChatInsertReqDto(from: "ssar", to: "cos", msg: "hello - ${random.nextInt(100)}");
+          chatController.insert(dto);
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 
